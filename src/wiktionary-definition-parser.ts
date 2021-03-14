bc525d947191d7e7f013993f8c71c34d0e47decc
Maroun Baydoun <maroun.baydoun@gmail.com>
2021-03-14 16:06:06 +0100

    Update dependencies

--- src/wiktionary-definition-parser.ts
+++ src/wiktionary-definition-parser.ts
@@ -1,45 +1,61 @@
-
 export const parse = (wikitext: string, startIndex: number): string[] => {
   return parseDefinitions(wikitext, startIndex);
-}
+};
 
-const parseDefinitions = (wikitext: string, startIndex: number, definitions: string[] = []): string[] => {
-  const newLineIndex = wikitext.indexOf('\n', startIndex);
-  const definition = wikitext.substring(startIndex, newLineIndex)
+const parseDefinitions = (
+  wikitext: string,
+  startIndex: number,
+  definitions: string[] = []
+): string[] => {
+  const newLineIndex = wikitext.indexOf("\n", startIndex);
+  const definition = wikitext
+    .substring(startIndex, newLineIndex)
     //Remove the leading # and space
-    .replace(/^#\s/, '')
+    .replace(/^#\s/, "")
     //Replace [[definition]] with `definition` and [[text | definition]] with `definition`
-    .replace(/\[\[(?:[\w\s]+\|)?([^,\]\[]+)\]\]/g, '$1')
+    .replace(/\[\[(?:[\w\s]+\|)?([^,\]\[]+)\]\]/g, "$1")
     //Replace {{gloss|word}} with (word)
-    .replace(/{{gloss\|([^{}]+)}}/g, '($1)')
+    .replace(/{{gloss\|([^{}]+)}}/g, "($1)")
     //Replace {{label|lang|word}} with `word`
-    .replace(/{{(?:lb|lbl|label)\|[\w-]+\|(.[^{}]+)}}/g, (_, label) => `(${label})`.replace(/\|/g, ', ').replace(/_,/g, ''))
+    .replace(/{{(?:lb|lbl|label)\|[\w-]+\|(.[^{}]+)}}/g, (_, label) =>
+      `(${label})`.replace(/\|/g, ", ").replace(/_,/g, "")
+    )
     //Replace {{link|lang|word}} with `word`
-    .replace(/{{(?:l|link)\|[\w-]+\|(.[^{}]+)}}/g, '$1')
+    .replace(/{{(?:l|link)\|[\w-]+\|(.[^{}]+)}}/g, "$1")
     //Replace {{non-gloss definition|word}} {{non-gloss|word}} and derivatives with word
-    .replace(/{{(?:non[-\s]gloss(?:\sdefinition)?|(?:n-g)|(?:ngd))\|(.[^{}]+)}}/g, '$1')
+    .replace(
+      /{{(?:non[-\s]gloss(?:\sdefinition)?|(?:n-g)|(?:ngd))\|(.[^{}]+)}}/g,
+      "$1"
+    )
     //Replace {{clipping of|lang|word}} with `Short for word`
-    .replace(/{{clipping of\|[\w-]+\|(.[^{}]+)}}/g, 'short for $1')
+    .replace(/{{clipping of\|[\w-]+\|(.[^{}]+)}}/g, "short for $1")
     //Replace {{short for|lang|word}} with `Short for word`
-    .replace(/{{short for\|[\w-]+\|(.[^{}]+)}}/g, 'short for $1')
+    .replace(/{{short for\|[\w-]+\|(.[^{}]+)}}/g, "short for $1")
     //Replace {{agent noun of|lang|verb}} with `Agent noun of verb`
-    .replace(/{{agent noun of\|[\w-]+\|(.[^{}]+)}}/g, 'agent noun of $1')
+    .replace(/{{agent noun of\|[\w-]+\|(.[^{}]+)}}/g, "agent noun of $1")
     //Replace {{alternative spelling of|lang|word}} with `Alternative spelling of word`
-    .replace(/{{alternative spelling of\|[\w-]+\|(.[^{}]+)}}/g, 'alternative spelling of $1')
+    .replace(
+      /{{alternative spelling of\|[\w-]+\|(.[^{}]+)}}/g,
+      "alternative spelling of $1"
+    )
     //Replace {{gerund of|lang|verb}} with `gerund of verb`
-    .replace(/{{gerund of\|[\w-]+\|(.[^{}]+)}}/g, 'gerund of $1')
+    .replace(/{{gerund of\|[\w-]+\|(.[^{}]+)}}/g, "gerund of $1")
     //Replace {{verbal noun of|lang|verb}} with `verbal noun of verb`
-    .replace(/{{verbal noun of\|[\w-]+\|(.[^{}]+)}}/g, 'verbal noun of $1')
+    .replace(/{{verbal noun of\|[\w-]+\|(.[^{}]+)}}/g, "verbal noun of $1")
     //Replace {{qualifier|text}} and its derivatives with (text)
-    .replace(/{{(?:qualifier|qual|q|i)\|([\w\s]+)}}/g, '($1)')
+    .replace(/{{(?:qualifier|qual|q|i)\|([\w\s]+)}}/g, "($1)")
     .trim();
 
-  if (newLineIndex === -1 || wikitext.charAt(newLineIndex + 1) !== '#') {
-    return [...definitions, definition].filter(def => def.match(/^(#[^\s]+)\s/) == null);
+  if (newLineIndex === -1 || wikitext.charAt(newLineIndex + 1) !== "#") {
+    return [...definitions, definition].filter(
+      (def) => def.match(/^(#[^\s]+)\s/) == null
+    );
   }
 
-  return parseDefinitions(wikitext, newLineIndex + 1, [...definitions, definition]);
-
-}
+  return parseDefinitions(wikitext, newLineIndex + 1, [
+    ...definitions,
+    definition,
+  ]);
+};
 
 export default parse;
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,41 @@
-const path = require('path');
+const { resolve } = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const PATHS = {
-  src: path.join(__dirname, './src'),
-  dist: path.join(__dirname, './dist')
-}
+  src: resolve(__dirname, "src"),
+  dist: resolve(__dirname, "dist"),
+};
 
 module.exports = {
-
   entry: {
-    'wiktionary-definition-parser': path.join(PATHS.src, 'wiktionary-definition-parser.ts'),
+    "wiktionary-definition-parser": resolve(
+      PATHS.src,
+      "wiktionary-definition-parser.ts"
+    ),
   },
   output: {
     path: PATHS.dist,
-    filename: '[name].js',
-    library: '[name]',
-    libraryTarget: 'umd',
+    filename: "[name].js",
+    library: "[name]",
+    libraryTarget: "umd",
     umdNamedDefine: true,
-    globalObject: 'this'
+    globalObject: "this",
   },
   module: {
     rules: [
       {
         test: /(\.ts)/,
-        loader: 'ts-loader',
-        exclude: /node_modules/
-      }
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [PATHS.dist, resolve(__dirname, "types")],
+    }),
+  ],
   resolve: {
-    extensions: ['.ts', '.js']
-  }
-}
+    extensions: [".ts", ".js"],
+  },
+};
